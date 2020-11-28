e880747579f583325ddeb73a178cc5f7779b0a76
usuario <usuario@Desktop-2316AG>
2020-11-28 14:04:04 +0100

    Ejercicio 6 , crear get , set y excepciones

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -2,5 +2,27 @@ package org.iesalandalus.programacion.alfilajedrez;
 
 public class Posicion {
 	private int fila;
-	private char columna;	
+	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila >= 1 && fila <= 8)
+			this.fila = fila;
+		else
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna >= 'a' && columna <= 'h')
+			this.columna = columna;
+		else
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+	}
 }
