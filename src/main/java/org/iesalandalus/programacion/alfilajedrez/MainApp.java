d3055db9e549951c374c3c9ae85c385c22bb3050
usuario <usuario@Desktop-2316AG>
2020-11-28 17:14:22 +0100

    Ejercicio 19.13 Método main

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -10,9 +10,11 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Alfil Ajedrez");
+		mostrarMenu();
+		
 	}
-
+	
 	private static void mostrarAlfil() {
 		System.out.println(alfil);
 	}
