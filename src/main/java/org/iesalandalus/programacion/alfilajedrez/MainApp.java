9151926c8553f2f35245c2c6f54e1750c1b0cd9d
usuario <usuario@Desktop-2316AG>
2020-11-28 16:58:47 +0100

    Ejercicio 19.3 Método elegirOpción

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 
 	private static Alfil alfil = null;
@@ -21,5 +23,22 @@ public class MainApp {
 		System.out.println("4. Mover el alfil.");
 		System.out.println("5. Salir.");
 
+	}
+	private static int elegirOpcion() {
+		int opcion;
+		System.out.println("Escoge una opción: ");
+		opcion = Entrada.entero();
+		while (opcion < 1 || opcion > 5) {
+			System.out.println("Opción incorrecta.");
+			System.out.println("\nEscoje una de las siguiente opciones: ");
+			System.out.println("1. Crear un alfil por defecto.");
+			System.out.println("2. Crear un alfil de un color. ");
+			System.out.println("3. Crear un alfil de un color en una columna inicial dada ('c' o 'f')");
+			System.out.println("4. Mover el alfil.");
+			System.out.println("5. Salir.");
+			System.out.println("Escoge una opción: ");
+			opcion = Entrada.entero();
+		}
+		return opcion;
 	}
 }
