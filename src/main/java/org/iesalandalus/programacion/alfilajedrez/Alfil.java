339e7273e326315e46110731a3532b72bf7de7cf
usuario <usuario@Desktop-2316AG>
2020-11-28 14:19:15 +0100

    Ejercicio 11 , crear la clase Alfil

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public class Alfil {
+
+	private Color color;
+	private Posicion posicion;
+
+	private int MIN_FILA = 1;
+	private int MAX_FILA = 8;
+	private char MIN_COLUMNA = 'a';
+	private char MAX_COLUMNA = 'h';
+
+}
