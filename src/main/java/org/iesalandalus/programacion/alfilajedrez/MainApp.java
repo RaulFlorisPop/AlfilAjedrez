a89b48361172a723fee262ce8f6d7acbd6c53eb9
usuario <usuario@Desktop-2316AG>
2020-11-28 16:57:28 +0100

    Ejericio 19.2 método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -12,4 +12,14 @@ public class MainApp {
 	private static void mostrarAlfil() {
 		System.out.println(alfil);
 	}
+
+	private static void mostrarMenu() {
+		System.out.println("\nEscoje una de las siguiente opciones: ");
+		System.out.println("1. Crear un alfil por defecto.");
+		System.out.println("2. Crear un alfil de un color. ");
+		System.out.println("3. Crear un alfil de un color en una columna inicial dada ('c' o 'f')");
+		System.out.println("4. Mover el alfil.");
+		System.out.println("5. Salir.");
+
+	}
 }
