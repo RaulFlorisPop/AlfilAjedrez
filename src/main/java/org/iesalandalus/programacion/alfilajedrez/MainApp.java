0e474991793ff8db37beaa7d46fcd1a8b1d415c4
usuario <usuario@Desktop-2316AG>
2020-11-28 16:55:52 +0100

    Ejercicio 19.1 método mostrarAlfil

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public class MainApp {
+
+	private static Alfil alfil = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void mostrarAlfil() {
+		System.out.println(alfil);
+	}
+}
